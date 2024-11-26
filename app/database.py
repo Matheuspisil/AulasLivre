1cc3942e1ebabdceb2491dff58d6c2915d65d205
MatheusPires-max <matheus@jabuti.ai>
2024-11-25 22:04:14 -0300

    Feature: atualização do database e do mains e criação dos requerimentos.

--- app/database.py
+++ app/database.py
@@ -1,11 +1,25 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import time
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./banco.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-Sessionlocal = Sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base
+connected = False
 
+while not connected:
+    try:
+        engine = engine = create_engine(SQLALCHEMY_DATABASE_URL)
+        connected = True
+    except Exception as e:
+        print(f"Erro ao tentar se conectar: {e}")
+        time.sleep(5)
+def get_db():
+    db = Sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import models, database, crud
+from app.database import get_db
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Aulas de Python"}
+
+@app.get("/usuarios/")
+def read_users(skip: int = 0, limit: int = 10 , db: Session = Depends(database.get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
